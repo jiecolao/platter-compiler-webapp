03a54a9a4f88b3f17691b25c5a00e1201c31f41a
Jieco Lao <jiecoandres.lao07@gmail.com>
2025-11-23 20:07:15 +0800

    Parser patch #4

--- backend/tests/test_parser.py
+++ backend/tests/test_parser.py
@@ -4,7 +4,7 @@ from app.parser.parser import Parser
 from tests.syntax_tscripts import SYNTAX_TSCRIPTS
 
 class TestParser():
-    
+
     def run_script(self):
         # choice = input("Enter test script number: ").strip()
         for script in SYNTAX_TSCRIPTS:
@@ -12,19 +12,26 @@ class TestParser():
             lexer = Lexer(code)
             tokens = lexer.tokenize()
             parser = Parser(tokens)
+            result = ""
 
             try:
                 result = parser.parse()
+                msg = (
+                    f"============ CODE #{script['number']} ================\n"
+                    f"CODE:\n{script['code']}\n"
+                    f"EXPECTED OUTPUT: {script['expected_output']}\n"
+                    f"SYNTAX OUTPUT: {"✔ No Syntax Error" if result else None}\n"
+                    f"=====================================\n"
+                )
             except SyntaxError as e:
-                print(str(e))
-
-            msg = (
-                f"============ CODE #{script['number']} ================\n"
-                f"CODE:\n{script['code']}\n"
-                f"EXPECTED OUTPUT: {script['expected_output']}\n"
-                f"SYNTAX OUTPUT: {result}\n"
-                f"=====================================\n"
-            )
+                msg = (
+                    f"============ CODE #{script['number']} ================\n"
+                    f"CODE:\n{script['code']}\n"
+                    f"EXPECTED OUTPUT: {script['expected_output']}\n"
+                    f"SYNTAX OUTPUT: {e}\n"
+                    f"=====================================\n"
+                )
+            
             print(msg)
 
 if __name__=="__main__":

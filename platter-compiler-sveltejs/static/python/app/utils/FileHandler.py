2cd6ace9d112636da2aa9b18ab839d0358d2a886
Jieco Lao <jiecoandres.lao07@gmail.com>
2026-02-02 10:33:17 +0800

    Moved utils

--- platter-compiler-sveltejs/static/python/app/utils/FileHandler.py
+++ platter-compiler-sveltejs/static/python/app/utils/FileHandler.py
@@ -1,8 +1,18 @@
-class FileHandler:
-    def __init__(filepath): pass
 
-    def run_file(filepath):
-        
-        with open(filepath, "r", encoding="utf-8") as f:
-            source = f.read()
-            return source
+import os
+
+# Get the directory where this file is located (app/utils/)
+samples_dir = os.path.dirname(os.path.abspath(__file__))
+
+def run_file(filepath):
+    # If filepath is just a filename, look for it in samples_dir
+    if not os.path.isabs(filepath) and os.sep not in filepath:
+        filepath = os.path.join(samples_dir, filepath)
+    
+    with open(filepath, "r", encoding="utf-8") as f:
+        source = f.read()
+        return source
+
+
+class FileHandler:
+    pass
